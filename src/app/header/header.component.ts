bdea8c8440d9f420d634032e6f3cbf6163dc29a8
Fred Caceres <fcaceres71@gmail.com>
2019-04-04 18:31:22 -0400

    fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+over;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
     .pipe(
       map(result => result.matches)
